fdbd2cfa173395249ca5ecf4801b4688fbc1011a
mdelsole <mdelsole64@gmail.com>
2019-12-11 21:41:59 -0500

    Updated format

--- gui.py
+++ gui.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template, request
 from sql_queries import *
+import csv
+
 
 app = Flask(__name__)
+
+# For images of the flowers
+with open("flowersImages.csv", 'r') as my_file:
+    reader = csv.reader(my_file, delimiter='\t')
+    listOfImages = list(reader)
 
 # Main page
 
@@ -9,7 +16,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     results = sql_query('SELECT * FROM FLOWERS')
-    return render_template('home.html', flowers=results)
+    return render_template('home.html', flowers=results, images=listOfImages)
 
 # Router for delete
 
@@ -22,6 +29,21 @@ def delete():
         comname = request.args.get('comname')
         sql_delete('DELETE FROM FLOWERS '
                    'WHERE GENUS = ? AND SPECIES = ? AND COMNAME = ?', (genus, species, comname))
+    results = sql_query('SELECT * FROM FLOWERS')
+    return render_template('home.html', flowers=results)
+
+
+@app.route('/select', methods=['POST', 'GET'])
+def select():
+    if request.method == 'GET':
+        genus = request.args.get('genus')
+        species = request.args.get('species')
+        comname = request.args.get('comname')
+        sql_update('SELECT *'
+                   'FROM SIGHTINGS '
+                   'WHERE GENUS = ? AND SPECIES = ? AND COMNAME = ?'
+                   'ORDER BY sighted DESC'
+                   'LIMIT 10', (genus, species, comname))
     results = sql_query('SELECT * FROM FLOWERS')
     return render_template('home.html', flowers=results)
 
